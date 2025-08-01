415bc622765fcbee090e5f1cc38e30bb3263f07c
RaghurajNagaraj <raghunagaraj1998@gmail.com>
2025-08-01 13:04:05 +0530

    practice of singleton

---
+++ com/java/practice/BankLogger.java
@@ -0,0 +1,26 @@
+package com.java.practice;
+
+// Singleton Logger Class
+public class BankLogger {
+
+    // Static instance
+    private static BankLogger instance;
+
+    // Private constructor
+    private BankLogger() {
+        System.out.println("Bank Logger initialized...");
+    }
+
+    // Public method to get the instance
+    public static BankLogger getInstance() {
+        if (instance == null) {
+            instance = new BankLogger();
+        }
+        return instance;
+    }
+
+    // Logging method
+    public void log(String message) {
+        System.out.println("[BANK LOG] " + message);
+    }
+}
---
+++ com/java/practice/BankingApp.java
@@ -0,0 +1,13 @@
+package com.java.practice;
+
+public class BankingApp {
+    public static void main(String[] args) {
+        BankLogger logger1 = BankLogger.getInstance();
+        logger1.log("Deposit of ₹5000 completed");
+
+        BankLogger logger2 = BankLogger.getInstance();
+        logger2.log("Loan approval for customer ID 12345");
+
+        System.out.println(logger1 == logger2);  // Output: true
+    }
+}
